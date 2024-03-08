d3e72a9ad9382b4d838ac70228872a841441be26
Edward Lopez <edtolo92@gmail.com>
2024-03-07 20:11:13 -0800

    covered quadratic time. the difference between two for loops sepeerate on the same indent vs two for loops nested and how the first is a + b and the second is a * b.

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-// FOUR RULES OF BIG O:
-// 1. Worst Case: Write code for the worst case scenario.
-// The value we are looking for is at the end of the array O(n)
+/*
 
-// 2. Remove Constants
+                            FOUR RULES OF BIG O:
+1. Worst Case: Write code for the worst case scenario.
+The value we are looking for is at the end of the array O(n)
 
-// 3. Different terms for inputs
+2. Remove Constants
 
-// 4. Drop Non Dominants
+3. Different terms for inputs
 
-// ============================================================
+4. Drop Non Dominants: 
 
-// Big O Notation: O(n) => loops over each box one by one
-// Time Complexity: Linear Time
+    O(x^2+3x+100+x/2) => O(x^2) => O(n^2) Quadratic Time
+    O(n^3) => cubed => Terrible Scaling Terrible Big O
+
+                                TIP: 
+    if two for loops are on the same indentation, it is O(n + m)
+if a for loop is within a for loop, it is O(n * n)
+*/
+
+// =========================  O(n) ==================================
+
+/* 
+Big O Notation: O(n) => loops over each box one by one
+Time Complexity: Linear Time
+*/
 
 const everyone = [
   "dory",
@@ -48,7 +60,7 @@ function compressAllBoxes(boxes) {
 
 compressAllBoxes(boxes);
 
-// ============================================================
+// ========================= O(1) ==============================
 
 // Big O Notation: O(1)
 // Time Complexity: Constant Time
@@ -73,7 +85,8 @@ function logFirstTwoBoxes(boxes3) {
 
 logFirstTwoBoxes(boxes3);
 
-// ============================================================
+// ========================  O(n + m) ===============================
+
 // Big O evaluations how many operations are happening...
 // Big O Notation: O(1 + n/2 + 100) => O(n);
 // Drop constants: O(n/2 + 100) => O(n + 1) => O(n)
@@ -110,3 +123,75 @@ const compressBoxesTwice = (boxes) => {
 };
 
 compressBoxesTwice(boxes);
+
+// Example 3:
+// Big O => O(n + m)
+// boxes 1 + boxes 2
+
+// const compressBoxesTwiceStill = (boxes, boxes2) => {
+//   boxes.forEach((boxes) => {
+//     console.log(boxes);
+//   });
+
+//   boxes2.forEach((boxes) => {
+//     console.log(boxes);
+//   });
+// };
+
+// compressBoxesTwiceStill(boxes);
+
+// ======================== O(n^2) ======================================
+// Big O Notation: O(loop * loop) => O(n ^ 2)
+// Time Complexity: Quadratic Time =>
+// 2 elements by 2 elements = 4 operations
+// 3 elements squared is 3 * 3 = 9 operations
+// A lot of interview questions ask us to convert from quadratic time to something faster
+
+const boxes_ = ["a", "b", "c", "d"];
+
+function logAllPairsOfArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(array[i], array[j]);
+    }
+  }
+}
+
+logAllPairsOfArray(boxes_);
+
+// Example 2:
+
+function logAllPairsOfBoxes(boxes) {
+  boxes.forEach((firstBox) => {
+    boxes.forEach((secondBox) => {
+      console.log(firstBox, secondBox);
+    });
+  });
+}
+
+logAllPairsOfBoxes(boxes_);
+
+// Example 3:
+
+// O(n + n^2) => drop non dominant terms(n) aka keep the most important term => O(n ^ 2)
+
+// const numbers = [1, 2, 3, 4];
+
+function printAllNumbersThenAllPairSums(numbers) {
+  console.log("these are the numbers:");
+  numbers.forEach((number) => {
+    console.log(number);
+  });
+
+  console.log("and these are their sums:");
+  numbers.forEach(function (firstNumber) {
+    numbers.forEach(function (secondNumber) {
+      console.log("pairing:", firstNumber, secondNumber);
+      console.log("sum of each pairing:", firstNumber + secondNumber);
+    });
+  });
+}
+
+printAllNumbersThenAllPairSums([10, 20, 30, 40, 50]);
+
+// ========================   ========================
